d98822a6da7719cca1f0d89f640ae5309c520d63
salmanke <55252719+salmanke@users.noreply.github.com>
2022-05-09 16:02:31 +0530

    Initial commit

---
+++ Arrays/leadersInArray.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    //considering arrays contains only positive elements
+    //not considering the order of leader elements
+
+    public static leadersInArray(int a[],int n){
+        int l_max=0,g_max=0;
+        for(int i=n-1;i>=0;i--){
+            l_max=a[i];
+            if(l_max>g_max){
+                System.out.print(l_max+" ");
+                g_max=l_max;
+            }
+        }
+    }
+}
